23c8406a40472ce5287826822099cd8e9bbab7e1
Noah Cornish <noahcornish7@gmail.com>
2023-12-14 15:20:17 -0500

    version 0.4.0

--- R/data.R
+++ R/data.R
@@ -5,6 +5,7 @@
 #library(OHLpkg)
 #x <- get_Stats()
 #y <- get_EVStats()
+#z <- get_DYStats()
 
 get_Stats <- function(LeagueStats) {
 
@@ -225,5 +226,112 @@ get_EVStats <- function(EVStats) {
     )
 
   return(EVStats)
+
+}
+
+
+
+get_DYStats <- function(DYStats){
+
+  library(rsconnect)
+  library(ggplot2)
+  library(tidyverse)
+  library(janitor)
+  library(lubridate)
+  library(RJSONIO)
+  library(jsonlite)
+  library(dplyr)
+  library(scales)
+
+  url_reg <- "https://lscluster.hockeytech.com/feed/?feed=modulekit&view=statviewtype&type=topscorers&key=2976319eb44abe94&fmt=json&client_code=ohl&lang=en&league_code=&season_id=76&first=0&limit=50000&sort=active&stat=all&order_direction="
+
+  # use jsonlite::fromJSON to handle NULL values
+  json_data <- jsonlite::fromJSON(url_reg, simplifyDataFrame = TRUE)
+
+
+  # create data frame
+  df <- json_data[["SiteKit"]][["Statviewtype"]] %>%
+    select(rank, player_id:num_teams) %>%
+    select(-c(birthtown, birthprov, birthcntry,
+              loose_ball_recoveries, caused_turnovers, turnovers,
+              phonetic_name, last_years_club, suspension_games_remaining,
+              suspension_indefinite)) %>%
+    mutate(player_id = as.numeric(player_id)) %>%
+    mutate(across(active:age, ~as.numeric(.))) %>%
+    mutate(across(rookie:jersey_number, ~as.numeric(.))) %>%
+    mutate(team_id = as.numeric(team_id)) %>%
+    mutate(across(games_played:faceoff_pct, ~as.numeric(.))) %>%
+    mutate(across(shots_on:num_teams, ~as.numeric(.))) %>%
+    mutate(birthdate_year = stringr::str_split(birthdate_year,
+                                               "\\'", simplify = TRUE, n = 2)[,2]) %>%
+    mutate(birthdate_year = as.numeric(birthdate_year)) %>%
+    mutate(birthdate_year = 2000 + birthdate_year)
+
+
+
+  # create data frame with columns required for tableau viz
+  LeagueStats <- df %>%
+    select(Name = "name",
+           Rookie = "rookie",
+           JN = "jersey_number",
+           BD = "birthdate",
+           BD_Y = "birthdate_year",
+           Hgt = "height",
+           Wgt = "weight",
+           Pos = "position",
+           Team = "team_name",
+           GP = "games_played",
+           G = "goals",
+           A = "assists",
+           PTS = "points",
+           `Pts/G` = "points_per_game",
+           `+/-` = "plus_minus",
+           PPG = "power_play_goals",
+           PPA = "power_play_assists",
+           PPP = "power_play_points",
+           SHG = "short_handed_goals",
+           SHA = "short_handed_assists",
+           SHPTS = "short_handed_points",
+           GWG = "game_winning_goals",
+           ENG = "empty_net_goals",
+           PIM = "penalty_minutes",
+           Active = "active") %>%
+    filter(Active == 1) %>%
+    filter(GP > 9)
+
+
+
+  LeagueStats <- LeagueStats %>%
+    mutate(PPP = PPG + PPA) %>%
+    mutate(`PPP_Percentage` = (PPP/PTS)*100) %>%
+    filter(Pos != "G")
+
+
+  PowerPlayPointsPerc <- LeagueStats %>%
+    na.omit(PowerPlayPointsPerc$PPP_Percentage)
+
+  #assign rookie values as YES or No instead of binary (1, 0)
+  LeagueStats$Rookie <- gsub('1', 'YES', LeagueStats$Rookie)
+  LeagueStats$Rookie <- gsub('0', 'NO', LeagueStats$Rookie)
+
+  LeagueStats$PPP_Percentage <- round(LeagueStats$PPP_Percentage ,digit=1)
+
+
+  #Ranking Players based on G, A, and +/-
+
+  LeagueStats <- LeagueStats %>%
+    mutate(RNK = (2*G)+(1.5*A)+(1*`+/-`)-(1*PIM))
+
+  LeagueStats$BD <- gsub(",", "", LeagueStats$BD)
+  LeagueStats$BD <- as.POSIXct(LeagueStats$BD, format='%B %d %Y')
+
+  LeagueStats$BD <- as.Date(LeagueStats$BD, format = "%d-%b-%Y")
+
+
+  DY <- LeagueStats %>%
+    filter(BD >= as.Date("2005-09-16") & BD <= as.Date("2006-09-15")) %>%
+    select(Name, Rookie, BD, Hgt, Wgt, Pos, Team,
+           GP, G, A, PTS, `Pts/G`, `+/-`, PPG,
+           PPA, PPP, GWG, ENG, PIM)
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OHLpkg
-# v0.3.2
+# v0.4.0
+
+This release has added NHL 2024 Draft Eligible Player Statistics for the OHL.
