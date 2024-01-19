360f2ce0e2b2f101f30082abcce853a1e4cb5252
Noah Cornish <noahcornish7@gmail.com>
2024-01-18 20:03:55 -0500

    Update OHL_MySQL_Upload.R
    Source files added.

--- R/OHL_MySQL_Upload.R
+++ R/OHL_MySQL_Upload.R
@@ -3,3 +3,25 @@
 # This file is used to upload dataframes to the MySQL server.
 
 
+library(DBI)
+library(RMariaDB)
+library(tidyverse)
+
+# source files to execute data frames
+source('R Files/OHL_DraftYear_Stats.R')
+source('R Files/OHL_EV_Stats.R')
+source('R Files/OHL_Goalie_Stats.R')
+source('R Files/OHL_Player_Stats.R')
+source('R Files/OHL_Rookie_Stats.R')
+source('R Files/OHL_SH_Stats.R')
+source('R Files/OHL_Schedule.R')
+source('R Files/OHL_Teams.R')
+
+
+
+
+
+
+
+
+
